4325f0e34a3ad9e50c3044cbaea067050726f387
CriCrin03 <chris012203@gmail.com>
2025-09-14 01:22:09 -0700

    done

--- app/src/main/java/elizalde/chris/thecheezery/MenuActivity.kt
+++ app/src/main/java/elizalde/chris/thecheezery/MenuActivity.kt
@@ -2,28 +2,22 @@ package elizalde.chris.thecheezery
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Telephony
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class RegisterActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_menu)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val button: Button = findViewById(R.id.sign_in_button)
-
-        button.setOnClickListener {
-            val intent: Intent = Intent (this, MenuActivity::class.java)
-            startActivity(intent)
         }
     }
 }
