7e7ed9ee7b98f47e1f63af9000e0101b8365ddeb
CriCrin03 <chris012203@gmail.com>
2025-09-16 22:32:32 -0700

    Completada con listas y diseño casi igual XD

--- app/src/main/java/elizalde/chris/thecheezery/MenuActivity.kt
+++ app/src/main/java/elizalde/chris/thecheezery/MenuActivity.kt
@@ -19,5 +19,54 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+        var btnSweet: Button = findViewById(R.id.button_sweets) as Button
+        var btnSalties: Button = findViewById(R.id.button_salties) as Button
+        var btnCombo: Button = findViewById(R.id.button_combos) as Button
+        var btnCustom: Button = findViewById(R.id.button_custom) as Button
+
+
+        btnCold.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipoMenu", "cold_drinks")
+            intent.putExtra("imgTipoProducto", R.drawable.colddrinks)
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipoMenu", "hot_drinks")
+            intent.putExtra("imgTipoProducto", R.drawable.hotdrinks)
+            startActivity(intent)
+        }
+
+        btnSweet.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipoMenu", "sweets")
+            intent.putExtra("imgTipoProducto", R.drawable.sweets)
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipoMenu", "salties")
+            intent.putExtra("imgTipoProducto", R.drawable.salties)
+            startActivity(intent)
+        }
+
+//        btnCombo.setOnClickListener{
+//            var intent: Intent = Intent(this, ProductosActivity::class.java)
+//            intent.putExtra("tipoMenu", "combo")
+//            startActivity(intent)
+//        }
+//
+//        btnCustom.setOnClickListener{
+//            var intent: Intent = Intent(this, ProductosActivity::class.java)
+//            intent.putExtra("tipoMenu", "custom")
+//            startActivity(intent)
+//        }
+
     }
 }
